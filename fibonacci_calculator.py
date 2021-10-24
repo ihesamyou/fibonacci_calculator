5e61caee6a4f5af915a5136d7562ecfa567e64a8
ihosseinu <ihosseinu@gmail.com>
2021-10-24 18:28:33 +0330

    fixed bugs related to rename

--- fibonacci_calculator.py
+++ fibonacci_calculator.py
@@ -4,18 +4,21 @@ def fibonacci_index(index):
     """returns fibonacci sequence with the given index being the last value.
     raises a type error if given index is a string, float, zero or negative number.
     returns a string for given indexes that are 1 and 2."""
+    try:
+        if type(index) == str or type(index) == float or index < 1:
+            raise TypeError
+        elif index < 3:
+            return f"""[0, 1]
+    For getting better results enter a whole number bigger than 2."""
+        else:
+            initial_sequence = [0, 1]
+            while len(initial_sequence) < index:
+                next = initial_sequence[-2] + initial_sequence[-1]
+                initial_sequence.append(next)
+            return initial_sequence
 
-    if type(index) == str or type(index) == float or index < 1:
+    except TypeError:
         raise TypeError('Please enter a positive whole number.')
-    elif index < 3:
-        return f"""[0, 1]
-For getting better results enter a whole number bigger than 2."""
-    else:
-        initial_sequence = [0, 1]
-        while len(initial_sequence) < index:
-            next = initial_sequence[-2] + initial_sequence[-1]
-            initial_sequence.append(next)
-        return initial_sequence
 
 
 def fibonacci_until(values_until):
@@ -23,18 +26,21 @@ def fibonacci_until(values_until):
     raises value error if the input is a string or is less than 0.
     for 0 <= value_until <= 1 returns a string."""
 
-    if type(values_until) == str or values_until < 0:
-        raise ValueError(
-            'Please enter a positive number since fibonacci sequence only includes zero and positive numbers.')
-    elif values_until > 1:
-        initial_sequence = [0, 1]
-        while initial_sequence[-1] + initial_sequence[-2] < values_until:
-            next = initial_sequence[-2] + initial_sequence[-1]
-            initial_sequence.append(next)
-        return initial_sequence
-    else:
-        return f"""[0, 1]
-For getting better results enter a number bigger than 1."""
+    try:
+        if type(values_until) == str or values_until < 0:
+            raise ValueError
+        elif values_until > 1:
+            initial_sequence = [0, 1]
+            while initial_sequence[-1] + initial_sequence[-2] < values_until:
+                next = initial_sequence[-2] + initial_sequence[-1]
+                initial_sequence.append(next)
+            return initial_sequence
+        else:
+            return f"""[0, 1]
+    For getting better results enter a number bigger than 1."""
+
+    except ValueError:
+        raise ValueError('Please enter a positive number since fibonacci sequence only includes zero and positive numbers.')
 
 
 # print(fibonacci_index(20))
--- test_fibonacci_calculator.py
+++ test_fibonacci_calculator.py
@@ -1,34 +1,36 @@
 import unittest
-import fibonacci_sequence
+import fibonacci_calculator
 
 
 class TestFibonacci(unittest.TestCase):
 
     def test_index(self):
-        self.assertRaises(TypeError, fibonacci_sequence.fibonacci_index, 'abc')
-        self.assertRaises(TypeError, fibonacci_sequence.fibonacci_index, 10.2)
-        self.assertRaises(TypeError, fibonacci_sequence.fibonacci_index, 0)
-        self.assertRaises(TypeError, fibonacci_sequence.fibonacci_index, -10)
-        self.assertEqual(fibonacci_sequence.fibonacci_index(2), """[0, 1]
-For getting better results enter a whole number bigger than 2.""")
-        self.assertEqual(fibonacci_sequence.fibonacci_index(9), [
+        self.assertRaises(
+            TypeError, fibonacci_calculator.fibonacci_index, 'abc')
+        self.assertRaises(
+            TypeError, fibonacci_calculator.fibonacci_index, 10.2)
+        self.assertRaises(TypeError, fibonacci_calculator.fibonacci_index, 0)
+        self.assertRaises(TypeError, fibonacci_calculator.fibonacci_index, -10)
+        self.assertEqual(fibonacci_calculator.fibonacci_index(2), """[0, 1]
+    For getting better results enter a whole number bigger than 2.""")
+        self.assertEqual(fibonacci_calculator.fibonacci_index(9), [
                          0, 1, 1, 2, 3, 5, 8, 13, 21])
 
     def test_values_until(self):
         self.assertRaises(
-            ValueError, fibonacci_sequence.fibonacci_until, 'abc')
+            ValueError, fibonacci_calculator.fibonacci_until, 'abc')
         self.assertRaises(
-            ValueError, fibonacci_sequence.fibonacci_until, -1)
-        self.assertEqual(fibonacci_sequence.fibonacci_until(24), [
+            ValueError, fibonacci_calculator.fibonacci_until, -1)
+        self.assertEqual(fibonacci_calculator.fibonacci_until(24), [
                          0, 1, 1, 2, 3, 5, 8, 13, 21])
-        self.assertEqual(fibonacci_sequence.fibonacci_until(24.5), [
+        self.assertEqual(fibonacci_calculator.fibonacci_until(24.5), [
                          0, 1, 1, 2, 3, 5, 8, 13, 21])
-        self.assertEqual(fibonacci_sequence.fibonacci_until(0), """[0, 1]
-For getting better results enter a number bigger than 1.""")
-        self.assertEqual(fibonacci_sequence.fibonacci_until(0.5), """[0, 1]
-For getting better results enter a number bigger than 1.""")
-        self.assertEqual(fibonacci_sequence.fibonacci_until(1), """[0, 1]
-For getting better results enter a number bigger than 1.""")
+        self.assertEqual(fibonacci_calculator.fibonacci_until(0), """[0, 1]
+    For getting better results enter a number bigger than 1.""")
+        self.assertEqual(fibonacci_calculator.fibonacci_until(0.5), """[0, 1]
+    For getting better results enter a number bigger than 1.""")
+        self.assertEqual(fibonacci_calculator.fibonacci_until(1), """[0, 1]
+    For getting better results enter a number bigger than 1.""")
 
 
 if __name__ == '__main__':
